26baab940efacbabc773ad8295885bd28e996005
Rdelhote <50595398+Rdelhote@users.noreply.github.com>
2020-05-08 10:17:09 +0200

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -3,42 +3,67 @@
 # calling library
 library(reshape2)
 
-#1. Get dataset from web
-filename <- "getdata_dataset.zip"
-if (!file.exists(filename)){
-        fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-        download.file(fileURL, filename, method="curl")
-}  
-if (!file.exists("UCI HAR Dataset")) { 
-        unzip(filename) 
+#1. Get dataset from web and read data
+rawDataDir <- "./rawData"
+rawDataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+rawDataFilename <- "rawData.zip"
+rawDataDFn <- paste(rawDataDir, "/", "rawData.zip", sep = "")
+dataDir <- "./data"
+
+if (!file.exists(rawDataDir)) {
+        dir.create(rawDataDir)
+        download.file(url = rawDataUrl, destfile = rawDataDFn)
+}
+if (!file.exists(dataDir)) {
+        dir.create(dataDir)
+        unzip(zipfile = rawDataDFn, exdir = dataDir)
 }
 
 #2. Merges the training and the test sets to create one data set
 # Activity labels & features
-activLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
-activLabels[,2] <- as.character(activLabels[,2])
+# train data
+x_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/X_train.txt"))
+y_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/Y_train.txt"))
+s_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/subject_train.txt"))
+
+# test data
+x_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/X_test.txt"))
+y_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/Y_test.txt"))
+s_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/subject_test.txt"))
+
+# merge {train, test} data
+x_data <- rbind(x_train, x_test)
+y_data <- rbind(y_train, y_test)
+s_data <- rbind(s_train, s_test)
+
+
+
+#3. load feature & activity info
+# feature info
 feat <- read.table("UCI HAR Dataset/features.txt")
-feat[,2] <- as.character(feat[,2])
 
-# Extract mean and standard deviation
-featSelection <- grep(".*mean.*|.*std.*", feat[,2])
-featSelection.names <- feat[featSelection,2]
-featSelection.names = gsub('-mean', 'Mean', featSelection.names)
-featSelection.names = gsub('-std', 'Std', featSelection.names)
-featSelection.names <- gsub('[-()]', '', featSelection.names)
+# activity labels
+a_label <- read.table("UCI HAR Dataset/activity_labels.txt")
+a_label[,2] <- as.character(a_label[,2])
 
-# Loading datasets
-train <- read.table("UCI HAR Dataset/train/X_train.txt")[featSelection]
-trainActiv <- read.table("UCI HAR Dataset/train/Y_train.txt")
-trainSubj <- read.table("UCI HAR Dataset/train/subject_train.txt")
-train <- cbind(trainSubj, trainActiv, train)
+# extract feature cols & names named 'mean, std'
+selectedCols <- grep("-(mean|std).*", as.character(feat[,2]))
+selectedColNames <- feat[selectedCols, 2]
+selectedColNames <- gsub("-mean", "Mean", selectedColNames)
+selectedColNames <- gsub("-std", "Std", selectedColNames)
+selectedColNames <- gsub("[-()]", "", selectedColNames)
 
-test <- read.table("UCI HAR Dataset/test/X_test.txt")[featSelection]
-testActiv <- read.table("UCI HAR Dataset/test/Y_test.txt")
-testSubj <- read.table("UCI HAR Dataset/test/subject_test.txt")
-test <- cbind(testSubj, testActiv, test)
+#4. Appropriately labels the data set with descriptive variable names.
+x_data <- x_data[selectedCols]
+allData <- cbind(s_data, y_data, x_data)
+colnames(allData) <- c("Subject", "Activity", selectedColNames)
 
-# final merge datasets & labels
-Dataasone <- rbind(train, test)
-colnames(Dataasone) <- c("subject", "activity", featSelection.names)
+allData$Activity <- factor(allData$Activity, levels = a_label[,1], labels = a_label[,2])
+allData$Subject <- as.factor(allData$Subject)
 
+# 5. creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# group by subject and activity and summarise using mean
+meltedData <- melt(allData, id = c("Subject", "Activity"))
+tidyData <- dcast(meltedData, Subject + Activity ~ variable, mean)
+
+write.table(tidyData, "./tidy_dataset.txt", row.names = FALSE, quote = FALSE)
